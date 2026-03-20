5a7c30d08b5cb1f433ae72616fed35d35e6423a4
JB1574 <mrln0367@leeds.ac.uk>
2026-03-20 10:05:51 +0000

    completion of task 1

---
+++ session-2/task1/structure.h
@@ -0,0 +1,14 @@
+#ifndef STRUCTURE_H
+#define STRUCTURE_H
+
+#define NAME_LENGTH 50
+#define NUM_MODULES 3
+
+
+typedef struct {
+    char name[NAME_LENGTH];  // Student's name
+    int id;                  // Student's ID number
+    float marks[NUM_MODULES]; // Marks for three modules
+} Student;
+
+#endif
